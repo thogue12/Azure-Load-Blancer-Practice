f68b4a017c988d7aeefebf1be50d6d690f32169f
Tim Hogue <hoguetimothy12@gmail.com>
2023-11-23 00:58:08 -0500

    commit to the main branch to test github actions

--- computing.tf
+++ computing.tf
@@ -60,3 +60,32 @@ resource "azurerm_linux_virtual_machine" "linux_vm2" {
   }
 }
 
+
+# #create control vm ansible
+resource "azurerm_linux_virtual_machine" "caontrol_vm" {
+  name                  = var.control_vm
+  resource_group_name   = azurerm_resource_group.resource_group.name
+  location              = azurerm_resource_group.resource_group.location
+  size                  = "Standard_B2s"
+  admin_username        = "vm"
+  network_interface_ids = [azurerm_network_interface.network_interface3.id]
+  user_data             = base64encode(templatefile("install_apache.tftpl", local.data_inputs))
+
+
+  admin_ssh_key {
+    username   = "vm"
+    public_key = file("AzureLoadBalancer.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+}
